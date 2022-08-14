9a2b0d9f7cf1898441da723872a7ccb6bbba959c
Yuta Kudo <yuta.basstb@gmail.com>
2022-08-14 15:48:14 +0900

    calc study added

--- Chapter1.playground/Contents.swift
+++ Chapter1.playground/Contents.swift
@@ -16,4 +16,26 @@ var ans = a+b+c
 // compound assign opration
 var km = 10
 km += 5
-km += 5 
+km += 5
+
+// type declaration
+var title = "test"
+var numvar = 200
+var numvar2 = 200.5
+
+var title_1:String
+var numvar_3:Int
+var numvar_4:Double
+
+title_1 = "test"
+numvar_3 = 200
+numvar_4 = 200.5
+
+// numvar_3 * numvar_4 // type error
+
+var numvar_5:Double
+var numvar_6:Double
+numvar_5=200
+numvar_6=200.5
+numvar_5*numvar_6 // type error solved
+
