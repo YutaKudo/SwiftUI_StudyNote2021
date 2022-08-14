4c52cc2d0273db75b761222fa01be5424c20682c
Yuta Kudo <yuta.basstb@gmail.com>
2022-08-14 15:39:03 +0900

    first commit to remote repo

--- Chapter1.playground/Contents.swift
+++ Chapter1.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
 var greeting = "Hello, swiftUI"
+
+// calc test
+// #1
+1 + 2
+    
+
+// input var
+var a = 1
+var b = 2
+var c = 3
+var ans = a+b+c
+
+// compound assign opration
+var km = 10
+km += 5
+km += 5 
