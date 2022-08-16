d90bb1f22adf7e67507d2726e02a8f43e872b0ba
Yuta Kudo <yuta.basstb@gmail.com>
2022-08-16 20:53:25 +0900

    add Chapter2 dir

---
+++ Chapter2/Chapter2/Chapter2UITests/Chapter2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Chapter2UITestsLaunchTests.swift
+//  Chapter2UITests
+//
+//  Created by 工藤優汰 on 2022/08/16.
+//
+
+import XCTest
+
+class Chapter2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ Chapter2_pj/Chapter2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chapter2
+//
+//  Created by 工藤優汰 on 2022/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
